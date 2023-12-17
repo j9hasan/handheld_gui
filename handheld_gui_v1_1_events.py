4822f8619948b963a4b06e6be926fefc276e28ba
Jubaid Hossen <jubaid.eee07@gmail.com>
2023-12-17 11:10:11 +0600

    first commit

---
+++ handheld_gui_v1_1_events.py
@@ -0,0 +1,92 @@
+
+def ssid_dropdown_vchanged(event_struct):
+    return
+
+
+def connect_function(event_struct):
+    return
+
+
+def scan_function(event_struct):
+    return
+
+
+def eeprom_connect(event_struct):
+    return
+
+
+def save_bt_(event_struct):
+    return
+
+
+def stop_bt_(event_struct):
+    return
+
+
+def start_bt_(event_struct):
+    return
+
+
+def audio_dd_value_changed(event_struct):
+    return
+
+
+def play_music(event_struct):
+    return
+
+
+def stop_music(event_struct):
+    return
+
+
+def kill_audio_task(event_struct):
+    return
+
+
+def search_music(event_struct):
+    return
+
+
+def bt_bitrate_dd_vchanged(event_struct):
+    return
+
+
+def write_rfid(event_struct):
+    return
+
+
+def rfid_baud_dd_vchanged(event_struct):
+    return
+
+
+def rfid_band_dd_vchanged(event_struct):
+    return
+
+
+def dummy_trigger_func(event_struct):
+    return
+
+
+def no_wifi_icon_clicked(event_struct):
+    return
+
+
+def wifi_icon_clicked(event_struct):
+    return
+
+
+def save_to_sd_btn_clicked(event_struct):
+    return
+
+
+def write_setup(event_struct):
+    return
+
+
+def merge_switch_click(event_struct):
+    return
+
+
+def beepon_clicked(event_struct):
+    return
+
